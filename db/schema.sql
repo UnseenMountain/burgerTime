f302b0d883b6868c926a976b6c6e21b0bafe39bc
Luke Klymshyn <unseenmountain@gmail.com>
2020-01-20 22:36:28 -0800

    Schema and seeds data have been created

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXSITS foodDB;
+
+CREATE DATABASE foodDB;
+
+USE foodDB;
+
+CREATE TABLE foods{
+
+     id INT NOT NULL AUTO_INCREMENT,
+    food_name VARCHAR (100) NULL,
+    devoured BOOLEAN(1) NULL,
+     PRIMARY key (id)
+}
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO foods (food, devoured)
+VALUES ("Wood fire tritip pizza", false);
+
+INSERT INTO foods (food, devoured)
+VALUES ("Onion ring wraped hotdogs", false);
+
+INSERT INTO foods (food, devoured)
+VALUES ("Weaster bacon cheeseburger", false);
+
+INSERT INTO foods (food, devoured)
+VALUES ("Bullet Cake", false);

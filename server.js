ff80b9fc484bafed10c976aa1850dbd5e2a1ac00
Luke Klymshyn <unseenmountain@gmail.com>
2020-03-27 12:03:56 -0700

    changed ports

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ var routes = require("./controllers/burger_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost ${PORT}")
+    console.log("Server listening on: http://localhost" + PORT)
 });
 
